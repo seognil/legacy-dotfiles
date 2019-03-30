2c48e38c5fd87b80f88e68424c2df16e8a914496
seognil LC <seognil@gmail.com>
2019-03-30 14:34:05 +0800

    update(ubuntu): refactoring install.sh

--- ubuntu/install-dev.sh
+++ ubuntu/install-dev.sh
@@ -1,60 +1,60 @@
 #!/bin/sh
 
 
-# * ---------------- source
+# * ---------------- add source and mirror
 
+# * apt
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/seognil-forker/Ubuntu-China-Source/self/ChangeSource.sh) 18 ali"
 
-# * ---------------- update system
+# * node 10 LTS
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+# sudo sed -i 's/deb.nodesource.com\/node_...x/mirrors.tuna.tsinghua.edu.cn\/nodesource\/deb_10.x\//g' /etc/apt/sources.list.d/nodesource.list
+sudo sed -i 's/deb.nodesource.com\/node_...x/mirrors.ustc.edu.cn\/nodesource\/deb\/node_10.x/g' /etc/apt/sources.list.d/nodesource.list
 
-sudo apt update -y && sudo apt upgrade -y && sudo apt full-upgrade -y
+# * yarn
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+# * update source
+sudo apt update
 
 # * ---------------- install zsh
 
 sudo apt install -y zsh
 
+# * silent install oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed -e 's/^\s*chsh -s/sudo chsh -s/g' -e 's/^\s*env\szsh.*$/#/g')"
 
-# * ---------------- install screenfetch
+# * ---------------- install soft
 
-sudo apt install -y screenfetch
-
-# * ---------------- install nvm
-
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | zsh
-
-# * ---------------- install node
-
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-sudo sed -i 's/deb.nodesource.com\/node_...x/mirrors.tuna.tsinghua.edu.cn\/nodesource\/deb_10.x\//g' /etc/apt/sources.list.d/nodesource.list
-# sudo sed -i 's/deb.nodesource.com\/node_...x/mirrors.ustc.edu.cn\/nodesource\/deb\/node_10.x/g' /etc/apt/sources.list.d/nodesource.list
-sudo apt update && sudo apt install -y nodejs
-
+sudo apt install -y git screenfetch tree
+sudo apt install -y nodejs yarn
 sudo apt install -y gcc g++ make
 
-# * ---------------- install yarn
+# * nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | zsh
 
-curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt update && sudo apt install -y yarn
+# * ---------------- update system
 
-# * ---------------- update system again
-
-sudo apt update -y && sudo apt upgrade -y && sudo apt full-upgrade -y
+sudo apt update
+sudo apt upgrade -y && sudo apt full-upgrade -y
 sudo apt autoremove -y && sudo apt clean -y
 
-# * ---------------- set nrm
+# * ---------------- npm global
 
+# * set nrm
 sudo npm i -g nrm --registry=https://registry.npm.taobao.org
 nrm use taobao
 
-# * ---------------- yarn global
+# * npm upgrade
+sudo npm i -g npm
 
+# * yarn
 sudo yarn global add cloc @vue/cli create-react-app http-server
 
 # * ---------------- config zsh
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/seognil/dotfiles/master/ubuntu/zsh-custom.sh)"
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/seognil/dotfiles/master/ubuntu/zsh-custom.sh)"
 
 # * ---------------- testing
 
@@ -75,7 +75,9 @@ sudo npm -g list --depth 0
 echo -n "yarn global: "
 sudo yarn global list
 
-# * ---------------- ending into zsh
+echo ""
+
+# * ---------------- ending dive into zsh
 
 echo "---------------- Almost finish ----------------"
 
